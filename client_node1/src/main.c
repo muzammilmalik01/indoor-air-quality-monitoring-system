bd3fc2c10512031845f972f0e9f18d1089116215
muzamilmalik01 <muzammil123malik@gmail.com>
2025-06-09 13:02:15 +0200

    * All sensors working * Added basic error handling * Added basic Sensor Data Validation

--- client_node1/src/main.c
+++ client_node1/src/main.c
@@ -1,192 +1,142 @@
-/*
- * Copyright (c) 2018 Peter Bigot Consulting, LLC
- * Copyright (c) 2018 Linaro Ltd.
- *
- * SPDX-License-Identifier: Apache-2.0
- */
-
 #include <zephyr/kernel.h>
 #include <zephyr/device.h>
 #include <zephyr/drivers/sensor.h>
 #include <zephyr/sys/printk.h>
 #include <zephyr/drivers/sensor/ccs811.h>
 #include <stdio.h>
+#include <zephyr/logging/log.h>
+#include "sensor/scd4x/scd4x.h"
 
-static bool app_fw_2;
+//TODO: Add CoAP Data Transmission.
+//TODO: Add data validation for both sensors. (DONE)
+//TODO: Modularize code for better readability and maintainability.
+//TODO: Retry mechanism for sensor data fetching in case of failure.
+//TODO: Add Sensor Status (bool scd41_ok) 
 
-static const char *now_str(void)
-{
-	static char buf[16]; /* ...HH:MM:SS.MMM */
-	uint32_t now = k_uptime_get_32();
-	unsigned int ms = now % MSEC_PER_SEC;
-	unsigned int s;
-	unsigned int min;
-	unsigned int h;
-
-	now /= MSEC_PER_SEC;
-	s = now % 60U;
-	now /= 60U;
-	min = now % 60U;
-	now /= 60U;
-	h = now;
-
-	snprintf(buf, sizeof(buf), "%u:%02u:%02u.%03u",
-		 h, min, s, ms);
-	return buf;
-}
-
-static int do_fetch(const struct device *dev)
-{
-	struct sensor_value co2, tvoc, voltage, current;
-	int rc = 0;
-	int baseline = -1;
-
-#ifdef CONFIG_APP_MONITOR_BASELINE
-	rc = ccs811_baseline_fetch(dev);
-	if (rc >= 0) {
-		baseline = rc;
-		rc = 0;
-	}
+#if !DT_HAS_COMPAT_STATUS_OKAY(sensirion_scd41)
+#error "No sensirion,scd4x compatible node found in the device tree"
 #endif
-	if (rc == 0) {
-		rc = sensor_sample_fetch(dev);
-	}
-	if (rc == 0) {
-		const struct ccs811_result_type *rp = ccs811_result(dev);
 
-		sensor_channel_get(dev, SENSOR_CHAN_CO2, &co2);
-		sensor_channel_get(dev, SENSOR_CHAN_VOC, &tvoc);
-		sensor_channel_get(dev, SENSOR_CHAN_VOLTAGE, &voltage);
-		sensor_channel_get(dev, SENSOR_CHAN_CURRENT, &current);
-		printk("\n[%s]: CCS811: %u ppm eCO2; %u ppb eTVOC\n",
-		       now_str(), co2.val1, tvoc.val1);
-		printk("Voltage: %d.%06dV; Current: %d.%06dA\n", voltage.val1,
-		       voltage.val2, current.val1, current.val2);
-#ifdef CONFIG_APP_MONITOR_BASELINE
-		printk("BASELINE %04x\n", baseline);
-#endif
-		if (app_fw_2 && !(rp->status & CCS811_STATUS_DATA_READY)) {
-			printk("STALE DATA\n");
-		}
-
-		if (rp->status & CCS811_STATUS_ERROR) {
-			printk("ERROR: %02x\n", rp->error);
-		}
-	}
-	return rc;
-}
-
-#ifndef CONFIG_CCS811_TRIGGER_NONE
-
-static void trigger_handler(const struct device *dev,
-			    const struct sensor_trigger *trig)
-{
-	int rc = do_fetch(dev);
-
-	if (rc == 0) {
-		printk("Triggered fetch got %d\n", rc);
-	} else if (-EAGAIN == rc) {
-		printk("Triggered fetch got stale data\n");
-	} else {
-		printk("Triggered fetch failed: %d\n", rc);
-	}
-}
-
-#endif /* !CONFIG_CCS811_TRIGGER_NONE */
-
-static void do_main(const struct device *dev)
-{
-	while (true) {
-		int rc = do_fetch(dev);
-
-		if (rc == 0) {
-			printk("Timed fetch got %d\n", rc);
-		} else if (-EAGAIN == rc) {
-			printk("Timed fetch got stale data\n");
-		} else {
-			printk("Timed fetch failed: %d\n", rc);
-			break;
-		}
-		k_msleep(1000);
-	}
-}
+const struct device *scd41 = DEVICE_DT_GET_ANY(sensirion_scd41);
+const struct device *ccs81 = DEVICE_DT_GET_ANY(ams_ccs811);
 
 int main(void)
 {
-	const struct device *const dev = DEVICE_DT_GET_ONE(ams_ccs811);
-	struct ccs811_configver_type cfgver;
-	int rc;
-
-	if (!device_is_ready(dev)) {
-		printk("Device %s is not ready\n", dev->name);
-		return 0;
+	bool scd41_ok = true;
+	bool ccs81_ok = true;
+	if (!device_is_ready(scd41)) {
+		printk("SCD41 device is not ready\n");
+		scd41_ok = false;
+		return -1;
 	}
-
-	printk("device is %p, name is %s\n", dev, dev->name);
-
-	rc = ccs811_configver_fetch(dev, &cfgver);
-	if (rc == 0) {
-		printk("HW %02x; FW Boot %04x App %04x ; mode %02x\n",
-		       cfgver.hw_version, cfgver.fw_boot_version,
-		       cfgver.fw_app_version, cfgver.mode);
-		app_fw_2 = (cfgver.fw_app_version >> 8) > 0x11;
+	if (!device_is_ready(ccs81)) {
+		printk("CCS811 device is not ready\n");
+		ccs81_ok = false;
+		return -1;
 	}
-
-#ifdef CONFIG_APP_USE_ENVDATA
-	struct sensor_value temp = { CONFIG_APP_ENV_TEMPERATURE };
-	struct sensor_value humidity = { CONFIG_APP_ENV_HUMIDITY };
-
-	rc = ccs811_envdata_update(dev, &temp, &humidity);
-	printk("ENV_DATA set for %d Cel, %d %%RH got %d\n",
-	       temp.val1, humidity.val1, rc);
-#endif
-
-#ifdef CONFIG_CCS811_TRIGGER
-	struct sensor_trigger trig = { 0 };
-
-#ifdef CONFIG_APP_TRIGGER_ON_THRESHOLD
-	printk("Triggering on threshold:\n");
-	if (rc == 0) {
-		struct sensor_value thr = {
-			.val1 = CONFIG_APP_CO2_MEDIUM_PPM,
-		};
-		rc = sensor_attr_set(dev, SENSOR_CHAN_CO2,
-				     SENSOR_ATTR_LOWER_THRESH,
-				     &thr);
-		printk("L/M threshold to %d got %d\n", thr.val1, rc);
-	}
-	if (rc == 0) {
-		struct sensor_value thr = {
-			.val1 = CONFIG_APP_CO2_HIGH_PPM,
-		};
-		rc = sensor_attr_set(dev, SENSOR_CHAN_CO2,
-				     SENSOR_ATTR_UPPER_THRESH,
-				     &thr);
-		printk("M/H threshold to %d got %d\n", thr.val1, rc);
-	}
-	trig.type = SENSOR_TRIG_THRESHOLD;
-	trig.chan = SENSOR_CHAN_CO2;
-#elif defined(CONFIG_APP_TRIGGER_ON_DATAREADY)
-	printk("Triggering on data ready\n");
-	trig.type = SENSOR_TRIG_DATA_READY;
-	trig.chan = SENSOR_CHAN_ALL;
-#else
-#error Unhandled trigger on
-#endif
-	if (rc == 0) {
-		rc = sensor_trigger_set(dev, &trig, trigger_handler);
-	}
-	if (rc == 0) {
-#ifdef CONFIG_APP_TRIGGER_ON_DATAREADY
-		while (true) {
-			k_sleep(K_FOREVER);
+	
+	struct sensor_value co2_41, co2_811, temo, humi, tvoc, voltage, current;
+	while (true)
+	{
+		if (sensor_sample_fetch(scd41) < 0)
+		{
+			printk("Failed to fetch sample from SCD41\n");
+			continue;
+			// message to Server for SCD41 not working
 		}
-#endif
-	}
-	printk("Trigger installation got: %d\n", rc);
-#endif /* CONFIG_CCS811_TRIGGER */
-	if (rc == 0) {
-		do_main(dev);
+		if (sensor_sample_fetch(ccs81) < 0)
+		{
+			printk("Failed to fetch sample from CCS811\n");
+			continue;
+			// message to Server for CCS811 not working
+		}
+		else{
+			printk("Fetched samples successfully from both sensors.\n");
+			// message to Server for sensors working.
+
+			// ***** Error hangling for SCD41 *****
+			if(sensor_channel_get(scd41, SENSOR_CHAN_CO2, &co2_41) < 0){
+				printk("Failed to get CO2 channel from SCD41\n");
+				// message to server: Cannot get CO2 Data from SCD41
+				continue;
+			}
+			if(sensor_channel_get(scd41, SENSOR_CHAN_AMBIENT_TEMP, &temo) < 0){
+				printk("Failed to get temperature from SCD41\n");
+				// message to server: Cannot get Temperature Data from SCD41
+				continue;
+			}
+			if(sensor_channel_get(scd41, SENSOR_CHAN_HUMIDITY, &humi) < 0){
+				printk("Failed to get Humidity from SCD41\n");
+				// message to server: Cannot get Humidity from SCD41
+				continue;
+			}
+
+			// ***** Error hangling for SCD41 ENDS *****
+
+			// ***** Error hangling for CCS81 *****
+			
+			if(sensor_channel_get(ccs81, SENSOR_CHAN_CO2, &co2_811) < 0){
+				printk("Failed to get eCO2 from CCS811\n");
+				continue;
+				// message to server: Cannot get eCO2 from CCS811
+			}
+
+			if(sensor_channel_get(ccs81, SENSOR_CHAN_VOC, &tvoc) < 0){
+				printk("Failed to get TVOC from CCS81\n");
+				// message to server: Cannot get TVOC from CCS811
+				continue;
+			}
+			if(sensor_channel_get(ccs81, SENSOR_CHAN_VOLTAGE, &voltage) < 0.0){
+				printk("Failed to get Voltage from CCS811\n");
+				// message to server: Cannot get Voltage from CCS811
+				continue;
+			}
+			if(sensor_channel_get(ccs81, SENSOR_CHAN_CURRENT, &current) < 0){
+				printk("Failed to get Current from CCS811\n");
+				// message to server: Cannot get Current from CCS811
+				continue;
+			}
+			// ***** Error hangling for CCS81 ENDS *****
+			else{
+				// Data validation for SCD41 and CCS811
+				if (temo.val1 < -40 || temo.val1 > 85){
+					printk("Invalid Temperature value from SCD41: %d.%06d C\n", temo.val1, temo.val2);
+					// continue;
+					//message to server: Invalid Temperature value from SCD41
+				}
+				if (humi.val1 < 0 || humi.val1 > 100){
+					printk("Invalid Humidity value from SCD41: %d.%06d %%\n", humi.val1, humi.val2);
+					// continue;
+					//message to server: Invalid Humidity value from SCD41
+				}
+				if (co2_41.val1 < 0 && co2_41.val1 > 5000){
+					printk("Invalid CO2 value from SCD41: %d\n", co2_41.val1);
+					// continue;
+					//message to server: Invalid CO2 value from SCD41
+					
+				}
+				if (co2_811.val1 < 400 && co2_811.val1 > 8192){
+					printk("Invalid eCO2 value from CCS811: %u\n", co2_811.val1);
+					continue;
+					//message to server: Invalid eCO2 value from CCS811
+				}
+				if (tvoc.val1 < 0 && tvoc.val1 > 1187){
+					printk("Invalid TVOC value from CCS811: %u\n", tvoc.val1);
+					continue;
+					//message to server: Invalid TVOC value from CCS811
+				}
+				printk("\nSCD41:\n");
+				printk("CO2: %d.%06d ppm, Temperature: %d.%06d C, Humidity: %d.%06d %%\n",
+					   co2_41.val1, co2_41.val2, temo.val1, temo.val2, humi.val1, humi.val2);
+				printk("\nCCS811:\n");
+				printk("%u ppm eCO2; %u ppb eTVOC\n",co2_811.val1, tvoc.val1);
+				printk("Voltage: %d.%06dV; Current: %d.%06dA\n", voltage.val1,
+					voltage.val2, current.val1, current.val2);
+				// CoAP message to server with the data
+			}
+
+		}
+		k_sleep(K_SECONDS(5));
 	}
 	return 0;
 }
